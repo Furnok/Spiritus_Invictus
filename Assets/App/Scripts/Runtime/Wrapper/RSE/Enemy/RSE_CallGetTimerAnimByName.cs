de037a92d9bd06c87f05f1310db428b4057c50af
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-10-01 16:21:12 +0200

    Fix: Rename Script For Enemy Behavior

--- Assets/App/Scripts/Runtime/Wrapper/RSE/Enemy/RSE_CallGetTimerAnimByName.cs
+++ Assets/App/Scripts/Runtime/Wrapper/RSE/Enemy/RSE_CallGetTimerAnimByName.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using Unity.Properties;
 
 #if UNITY_EDITOR
-[CreateAssetMenu(menuName = "Behavior/Event Channels/CallGetTimerAnimByName")]
+[CreateAssetMenu(fileName = "RSE_CallGetTimerAnimByName", menuName = "Data/RSE/Enemy/CallGetTimerAnimByName")]
 #endif
 [Serializable, GeneratePropertyBag]
 [EventChannelDescription(name: "CallGetTimerAnimByName", message: "Get timer animatio by [Name]", category: "Events", id: "6ed3f6dc35d517aedd38436137c1ca59")]
-public sealed partial class CallGetTimerAnimByName : EventChannel<string> { }
+public sealed partial class RSE_CallGetTimerAnimByName : EventChannel<string> { }
 
