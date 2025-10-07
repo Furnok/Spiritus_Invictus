42caf478f0125a47c9fba9fd088ef0023b63578a
N3crom <mathias.manenq@gmail.com>
2025-10-07 16:54:34 +0200

    feat: start dodge

--- Assets/App/Scripts/Runtime/Player/S_PlayerDodge.cs
+++ Assets/App/Scripts/Runtime/Player/S_PlayerDodge.cs
@@ -2,21 +2,46 @@
 
 public class S_PlayerDodge : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private SSO_PlayerStateTransitions _ssoPlayerStateTransitions;
+
     [Header("Input")]
     [SerializeField] private RSE_OnPlayerDodgeInput rseOnPlayerDodge;
+    [SerializeField] private RSE_OnPlayerMove _rseOnPlayerMove;
+
+    Vector2 _moveInput;
 
     private void OnEnable()
     {
         rseOnPlayerDodge.action += TryDodge;
+        _rseOnPlayerMove.action += Move;
     }
 
     private void OnDisable()
     {
         rseOnPlayerDodge.action -= TryDodge;
+        _rseOnPlayerMove.action -= Move;
     }
 
     private void TryDodge()
     {
+        // Check if can dodge
+        if (true)
+        {
+            if(_moveInput != Vector2.zero)
+            {
+                // Dodge in direction of the movement input
+            }
+            else
+            {
+                // Backflip
+            }
 
+        }
+    }
+
+    private void Move(Vector2 input)
+    {
+        _moveInput = input;
     }
 }
