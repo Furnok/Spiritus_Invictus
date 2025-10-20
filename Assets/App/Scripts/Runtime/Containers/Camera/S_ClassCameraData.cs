dd065695b9124d117e5b94804d5722a27ebd334c
Killian FURNO <killian.furno@orange.fr>
2025-10-20 14:15:16 +0200

    Fix: Camera

--- Assets/App/Scripts/Runtime/Containers/Camera/S_ClassCameraData.cs
+++ Assets/App/Scripts/Runtime/Containers/Camera/S_ClassCameraData.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Sirenix.OdinInspector;
+using System;
 
 [Serializable]
 public class S_ClassCameraData
 {
+    [Title("Camera")]
     public float cameraDistanceMinPlayer;
-    public float fadeSpeedPlayer;
     public float rotationCameraPlayerDuration;
+
+    [Title("Player")]
+    public float fadeSpeedPlayer;
 }
