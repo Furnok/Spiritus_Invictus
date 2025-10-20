5e42b8e06e6c5b22107567aae8788e7e0667e337
N3crom <mathias.manenq@gmail.com>
2025-10-20 22:56:31 +0200

    feat: debug on slider bar test

--- Assets/App/Scenes/Tests/Mathias/Test/TestConvictionBarUi.cs
+++ Assets/App/Scenes/Tests/Mathias/Test/TestConvictionBarUi.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,11 @@ public class TestConvictionBarUi : MonoBehaviour
     [Header("References")]
     [SerializeField] private SSO_PlayerConvictionData _playerConvictionData;
     [SerializeField] private RSO_PlayerCurrentConviction _playerCurrentConviction;
+    [SerializeField] private SSO_PlayerAttackSteps _playerAttackSteps;
     [SerializeField] Slider _sliderConviction;
+    [SerializeField] RectTransform _sliderFillAreaRectTransform;
+    [SerializeField] private RectTransform _ticksParent;
+    [SerializeField] private GameObject _tickPrefab;
 
     //[Header("Input")]
 
@@ -18,6 +23,8 @@ public class TestConvictionBarUi : MonoBehaviour
     {
         _sliderConviction.maxValue = _playerConvictionData.Value.maxConviction;
         _sliderConviction.value = _sliderConviction.maxValue;
+
+        StartCoroutine(BuildTicksNextFrame());
     }
 
     private void OnEnable()
@@ -34,4 +41,47 @@ public class TestConvictionBarUi : MonoBehaviour
     {
         _sliderConviction.value = health;
     }
+
+    private IEnumerator BuildTicksNextFrame()
+    {
+        yield return null;
+        CreateStepTicks();
+    }
+
+    void CreateStepTicks()
+    {
+        for (int i = _ticksParent.childCount - 1; i >= 0; i--)
+            Destroy(_ticksParent.GetChild(i).gameObject);
+
+        float maxConv = _playerConvictionData.Value.maxConviction;
+
+        var fillRect = _sliderFillAreaRectTransform;
+        if (_ticksParent != fillRect)
+        {
+            _ticksParent.SetParent(fillRect, worldPositionStays: false);
+            _ticksParent.anchorMin = new Vector2(0, 0.5f);
+            _ticksParent.anchorMax = new Vector2(1, 0.5f);
+            _ticksParent.pivot = new Vector2(0.5f, 0.5f);
+            _ticksParent.offsetMin = Vector2.zero;
+            _ticksParent.offsetMax = Vector2.zero;
+            _ticksParent.anchoredPosition = Vector2.zero;
+        }
+
+        foreach (var step in _playerAttackSteps.Value)
+        {
+            //if (step.ammountConvitionNeeded <= 0f || step.ammountConvitionNeeded > maxConv)
+            //    continue;
+
+            float normalized = Mathf.Clamp01(step.ammountConvitionNeeded / maxConv);
+
+            var tickGO = Instantiate(_tickPrefab, _ticksParent);
+            var rt = (RectTransform)tickGO.transform;
+
+            rt.anchorMin = new Vector2(normalized, 0.5f);
+            rt.anchorMax = new Vector2(normalized, 0.5f);
+            rt.pivot = new Vector2(0.5f, 0.5f);
+            rt.anchoredPosition = Vector2.zero;
+        }
+    }
+    
 }
