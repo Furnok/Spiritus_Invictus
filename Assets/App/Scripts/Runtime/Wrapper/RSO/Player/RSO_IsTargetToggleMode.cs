ec368c9c0dedc2169f1830b72de29b1cca39e5d9
N3crom <mathias.manenq@gmail.com>
2025-10-21 14:25:22 +0200

    feat: toggle mode on target systems

---
+++ Assets/App/Scripts/Runtime/Wrapper/RSO/Player/RSO_IsTargetToggleMode.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSO_IsTargetToggleMode", menuName = "Data/RSO/Player/RSO_IsTargetToggleMode")]
+public class RSO_IsTargetToggleMode : BT.ScriptablesObject.RuntimeScriptableObject<bool>{}
