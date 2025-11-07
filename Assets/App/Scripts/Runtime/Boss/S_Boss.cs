15c799e931a242a3b3bb2696521f1299329a7dbe
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-11-07 17:07:31 +0100

    Fix: Boss Animator

--- Assets/App/Scripts/Runtime/Boss/S_Boss.cs
+++ Assets/App/Scripts/Runtime/Boss/S_Boss.cs
@@ -1,11 +1,36 @@
+using Sirenix.OdinInspector;
+using Unity.Behavior;
 using UnityEngine;
 
 public class S_Boss : MonoBehaviour
 {
-    //[Header("Settings")]
+    [TabGroup("Settings")]
+    [Title("Animations Parameters")]
+    [SerializeField, S_AnimationName] private string moveParam;
 
-    //[Header("References")]
+    [TabGroup("Settings")]
+    [SerializeField, S_AnimationName] private string deathParam;
 
+    [TabGroup("Settings")]
+    [SerializeField, S_AnimationName] private string attackParam;
+
+    [TabGroup("Settings")]
+    [SerializeField, S_AnimationName] private string stunParam;
+
+    [TabGroup("References")]
+    [Title("Agent")]
+    [SerializeField] private BehaviorGraphAgent behaviorAgent;
+
+    [TabGroup("References")]
+    [Title("Colliders")]
+    [SerializeField] private Collider bodyCollider;
+
+    [TabGroup("References")]
+    [SerializeField] private Collider detectionCollider;
+
+    [TabGroup("References")]
+    [Title("Animator")]
+    [SerializeField] private Animator animator;
     //[Header("Inputs")]
 
     //[Header("Outputs")]
