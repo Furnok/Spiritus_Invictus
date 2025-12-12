cae0f07cc8d128cb2ab0caf838b44ec951f53e88
Killian FURNO <killian.furno@orange.fr>
2025-12-12 17:10:06 +0100

    Fix: Data Folder

--- Assets/App/Scripts/Runtime/Wrapper/RSE/Game/RSE_OnRumbleRequested.cs
+++ Assets/App/Scripts/Runtime/Wrapper/RSE/Game/RSE_OnRumbleRequested.cs
@@ -1,4 +1,4 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RSE_OnRumbleRequested", menuName = "Data/RSE/Game/RSE_OnRumbleRequested")]
+[CreateAssetMenu(fileName = "RSE_OnRumbleRequested", menuName = "Data/RSE/Game/OnRumbleRequested")]
 public class RSE_OnRumbleRequested : BT.ScriptablesObject.RuntimeScriptableEvent<S_StructRumbleData>{}
