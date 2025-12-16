2e6ea174aca135289807eb2f6fe2715549985c0c
Killian FURNO <killian.furno@orange.fr>
2025-12-16 14:09:45 +0100

    Fix: Material Instanciate

---
+++ Assets/App/Scripts/Runtime/Wrapper/RSE/Player/RSE_OnUpdateVisibility.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_OnUpdateVisibility", menuName = "Data/RSE/Player/OnUpdateVisibility")]
+public class RSE_OnUpdateVisibility : BT.ScriptablesObject.RuntimeScriptableEvent<float>{}
